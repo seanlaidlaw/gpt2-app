e26fd5b6679a18f7e26952ccc9dbc8d0eab98f6f
Sean LAIDLAW <seanlaidlaw95@gmail.com>
2021-02-26 21:34:43 +0000

    update download_model script to openai one that works

--- app/main/gpt2/download_model.py
+++ app/main/gpt2/download_model.py
@@ -3,24 +3,22 @@ import sys
 import requests
 from tqdm import tqdm
 
-basedir = os.path.dirname(os.path.abspath(__file__))
-
 if len(sys.argv) != 2:
-    print('You must enter the model name as a parameter, e.g.: download_model.py 117M')
+    print('You must enter the model name as a parameter, e.g.: download_model.py 124M')
     sys.exit(1)
 
 model = sys.argv[1]
 
 subdir = os.path.join('models', model)
-subdir_abs = os.path.join(basedir, subdir)
-if not os.path.exists(subdir_abs):
-    os.makedirs(subdir_abs)
+if not os.path.exists(subdir):
+    os.makedirs(subdir)
+subdir = subdir.replace('\\','/') # needed for Windows
 
 for filename in ['checkpoint','encoder.json','hparams.json','model.ckpt.data-00000-of-00001', 'model.ckpt.index', 'model.ckpt.meta', 'vocab.bpe']:
 
-    r = requests.get("https://storage.googleapis.com/gpt-2/" + subdir + "/" + filename, stream=True)
+    r = requests.get("https://openaipublic.blob.core.windows.net/gpt-2/" + subdir + "/" + filename, stream=True)
 
-    with open(os.path.join(subdir_abs, filename), 'wb') as f:
+    with open(os.path.join(subdir, filename), 'wb') as f:
         file_size = int(r.headers["content-length"])
         chunk_size = 1000
         with tqdm(ncols=100, desc="Fetching " + filename, total=file_size, unit_scale=True) as pbar:
